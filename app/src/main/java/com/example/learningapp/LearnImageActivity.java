485a40fc183d5884fbefb219fc4c656920145c32
Nosheen17 <hayat.mehreen17@gmail.com>
2022-05-20 08:59:23 +0500

    Declaring Intect

--- app/src/main/java/com/example/learningapp/LearnImageActivity.java
+++ app/src/main/java/com/example/learningapp/LearnImageActivity.java
@@ -14,7 +14,15 @@ public class LearnImageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn_image);
-      
+        Intent intent = getIntent();
+        int image = intent.getIntExtra("Image",-1);
+        if(image == -1){
+            //Error Loading
+        }
+        Log.d("NumberSecond",Integer.toString(image));
+        imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(image);
+
 
     }
 }
