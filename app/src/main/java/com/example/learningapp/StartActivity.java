67de086c7ed40f00012eda30b885c8330747bb87
Nosheen17 <hayat.mehreen17@gmail.com>
2022-05-20 10:22:29 +0500

    Cahnging the launcher from by default to second activity

--- app/src/main/java/com/example/learningapp/StartActivity.java
+++ app/src/main/java/com/example/learningapp/StartActivity.java
@@ -2,13 +2,26 @@ package com.example.learningapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
-
+ Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        btn=findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(StartActivity.this,MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
