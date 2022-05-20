71294b07b4c26ccc4f2fb6c31f6ce4022d5d0cfb
Nosheen17 <hayat.mehreen17@gmail.com>
2022-05-20 08:57:17 +0500

    Making Learning Image xml page

---
+++ app/src/main/java/com/example/learningapp/LearnImageActivity.java
@@ -0,0 +1,20 @@
+package com.example.learningapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+public class LearnImageActivity extends AppCompatActivity {
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learn_image);
+      
+
+    }
+}
