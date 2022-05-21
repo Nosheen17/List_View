0b106d1f2368abc29668eb1b6f9ea9ee7cef1919
Nosheen17 <hayat.mehreen17@gmail.com>
2022-05-21 12:48:54 +0500

    Added Test Pictures and Logic

--- app/src/main/java/com/example/learningapp/StartActivity.java
+++ app/src/main/java/com/example/learningapp/StartActivity.java
@@ -8,12 +8,20 @@ import android.view.View;
 import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
- Button btn;
+ Button btn,btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
         btn=findViewById(R.id.button);
+        btn1=findViewById(R.id.button2);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(StartActivity.this,TestActivty.class);
+                startActivity(intent);
+            }
+        });
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
